cfb4433394f955a6daed823b318c0fa57d66874a
MuhammadbinSajid <mohammedsajidamazai@gmail.com>
2024-10-26 17:08:52 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+country_code = {'India' : '0091', 'Austrailia' : '0025', 'Nepal' : '00977'}
+
+#Search dictionary for country code of India
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+
+#Search dictionary for country code of Japan
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
